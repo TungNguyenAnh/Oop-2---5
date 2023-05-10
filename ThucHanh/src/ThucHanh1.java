ade69388603709d2fa08530e7bd37dbe702cec66
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2023-05-10 16:14:07 +0700

    ThucHanhVaBaiTap

---
+++ ThucHanh/src/ThucHanh1.java
@@ -0,0 +1,6 @@
+// Lớp file được kế thừa từ lớp Object
+//Lớp file được thực thi từ lớp Srializable, Comparable
+//tao ra lop file de co the de dang truy cap, sap xep, su dung va quan li
+//Lớp file có 4 cách khởi tạo: file(file parent, String child); file(String pathname); File(String parent, String child)
+//Các phương thức: Tên, input, output, mục đích
+//....
